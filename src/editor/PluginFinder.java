f211c0bff0aa81bdf092234ebd4394546d3671bb
arnaudcoj <arnaud.cojez@etudiant.univ-lille1.fr>
2014-11-29 17:29:09 +0100

    init

---
+++ src/editor/PluginFinder.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package editor;
+
+/**
+ * Class defining the 
+ */
+public class PluginFinder {
+
+	/**
+	 * Constructor for the PluginFinder class
+	 */
+	public PluginFinder() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
