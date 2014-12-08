60e77098dd6c4da8c4ad5b5e6faa33131990b156
arnaudcoj <arnaud.cojez@etudiant.univ-lille1.fr>
2014-12-08 20:14:35 +0100

    init

---
+++ src/plugins/ToUpperPlugin.java
@@ -0,0 +1,18 @@
+package plugins;
+
+public class ToUpperPlugin implements Plugin {
+	@Override
+	public String transform(String s) {
+		return s.toUpperCase();
+	}
+
+	@Override
+	public String getLabel() {
+		return "toUpper";
+	}
+
+	@Override
+	public String helpMessage() {
+		return "Modifies the selection with uppercases";
+	}
+}
